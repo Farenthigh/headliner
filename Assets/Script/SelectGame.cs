efbfedc67e0f49388822f9f7daf9df7c9185ed87
Farenthigh <naxx.iiax@gmail.com>
2025-12-19 16:15:09 +0700

    first commit

---
+++ Assets/Script/SelectGame.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SelectGame : MonoBehaviour
+{
+    [SerializeField] private Button savinggame;
+    [SerializeField] private Button taxgame;
+    [SerializeField] private Button investgame;
+    
+    private void Start()
+    {
+        savinggame.onClick.AddListener(() => SelectGameOption("Saving Game"));
+        taxgame.onClick.AddListener(() => SelectGameOption("Tax Game"));
+        investgame.onClick.AddListener(() => SelectGameOption("Investment Game"));
+    }
+    private void SelectGameOption(string game)
+    {
+        Debug.Log($"Selected Game: {game}");
+    }
+}
