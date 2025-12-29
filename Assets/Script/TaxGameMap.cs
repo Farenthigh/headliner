e80374c25319956cc281cdee27101d19dbac1fb6
jirat <jiratchayaya303@gmail.com>
2025-12-30 00:50:09 +0700

    30122025_add select stage tax game map

--- Assets/Script/TaxGameMap.cs
+++ Assets/Script/TaxGameMap.cs
@@ -2,15 +2,24 @@ using UnityEngine;
 
 public class TaxGameMap : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+
+    [SerializeField] private Button stageButton1;
+    [SerializeField] private Button stageButton2;
+    [SerializeField] private Button stageButton3;
+    [SerializeField] private Button stageButton4;
+    [SerializeField] private Button stageButton5;
+
+    private void Start()
     {
-        
+        stageButton1.onClick.AddListener(() => TaxGameMapOption("Stage 1"));
+        stageButton2.onClick.AddListener(() => TaxGameMapOption("Stage 2"));
+        stageButton3.onClick.AddListener(() => TaxGameMapOption("Stage 3"));
+        stageButton4.onClick.AddListener(() => TaxGameMapOption("Stage 4"));
+        stageButton5.onClick.AddListener(() => TaxGameMapOption("Stage 5"));
+    }
+    private void TaxGameMapOption(string stageNumber)
+    {
+        Debug.Log($"Tax Game Map Option Selected: {stageNumber}");
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
