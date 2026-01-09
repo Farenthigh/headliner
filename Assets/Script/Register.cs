4f06977019c35bb740136665273d5e75c171fc50
Farenthigh <naxx.iiax@gmail.com>
2026-01-10 04:15:12 +0700

    Implement APIManager for user authentication and character selection; update login and register scripts to use email instead of username.

--- Assets/Script/Register.cs
+++ Assets/Script/Register.cs
@@ -1,23 +1,51 @@
 using UnityEngine;
 using TMPro;
 using UnityEngine.UI;
-public class Register : MonoBehaviour{
+public class Register : MonoBehaviour
+{
     [SerializeField] private TMP_InputField emailInput;
     [SerializeField] private TMP_InputField passwordInput;
     [SerializeField] private TMP_InputField confirmpasswordInput;
     [SerializeField] private Button loginButton;
     [SerializeField] private Button registerButton;
 
-    private void Start(){
+    private void Start()
+    {
         registerButton.onClick.AddListener(HandleRegister);
+        loginButton.onClick.AddListener(HandleLogin);
+    }
+    private void HandleLogin()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("LoginScene");
     }
 
-    private void HandleRegister(){
+    private async void HandleRegister()
+    {
         string email = emailInput.text;
         string password = passwordInput.text;
         string confirmpass = confirmpasswordInput.text;
 
-        Debug.Log($"Register with email: {email} and password: {password}");
+        if (password != confirmpass)
+        {
+            Debug.Log("Passwords do not match");
+            return;
+        }
+        try
+        {
+            await APIManager.Instance.Register(new RegisterStruct
+            {
+                email = email,
+                password = password,
+                confirm_password = confirmpass
+            });
+
+            UnityEngine.SceneManagement.SceneManager.LoadScene("LoginScene");
+        }
+        catch (System.Exception ex)
+        {
+            Debug.Log("Registration failed: " + ex.Message);
+        }
+
     }
 
 }
