86e880cd01b1400cfe0eef49c6a66858fe4f472f
Farenthigh <naxx.iiax@gmail.com>
2026-01-16 04:22:44 +0700

    feat: deposit & comment for new task

--- Assets/Script/BankScript.cs
+++ Assets/Script/BankScript.cs
@@ -1,12 +1,63 @@
-using Unity.VisualScripting;
+using System.Collections.Generic;
 using UnityEngine;
 
+public class Transaction
+{
+    public enum TransactionType { Deposit, Withdraw }
+    public TransactionType type;
+    public float amount;
+    public int roundMonth;
+    public int transactionMonth;
+    public int transactionYear;
+    public int currentMonth;
+    public bool isActive = true;
+    public bool isInterestPaid = false;
+    public bool isContractBroken = false;
+
+    public Transaction(TransactionType transactionType, float transactionAmount, int transactionRoundMonth, int transactionMonth, int transactionYear, int currentMonth)
+    {
+        type = transactionType;
+        amount = transactionAmount;
+        this.roundMonth = transactionRoundMonth;
+        this.transactionMonth = transactionMonth;
+        this.transactionYear = transactionYear;
+        this.currentMonth = currentMonth;
+    }
+}
 public class BankScript : MonoBehaviour
 {
-    // [SerializeField] private string bankName;
+    [SerializeField] private string bankName;
+    private List<Transaction> transactions = new List<Transaction>();
     private void OnMouseDown()
     {
         if (SavingGameUIManager.Instance.GetBankPanel() != true)
-            SavingGameUIManager.Instance.OnOpenBankPanel(gameObject.name);
+            SavingGameUIManager.Instance.OnOpenBankPanel(this);
+    }
+    public float GetBalance()
+    {
+        float balance = 0f;
+        foreach (Transaction transaction in transactions)
+        {
+            if (transaction.type == Transaction.TransactionType.Deposit)
+            {
+                balance += transaction.amount;
+            }
+            else if (transaction.type == Transaction.TransactionType.Withdraw)
+            {
+                balance -= transaction.amount;
+            }
+        }
+        return balance;
+    }
+    public void Deposit(float amount)
+    {
+        Transaction newTransaction = new Transaction(Transaction.TransactionType.Deposit, amount, 0, SavingGameLogicManager.Instance.GetCurrentMonth(), SavingGameLogicManager.Instance.GetCurrentYear(), SavingGameLogicManager.Instance.GetCurrentMonth());
+        SavingGameLogicManager.Instance.DeductCash(amount);
+        transactions.Add(newTransaction);
+    }
+    public void Withdraw(float amount)
+    {
+        //TODO: Implement withdraw functionality //eve
+        //wallet += withdraw
     }
 }
--- Assets/Script/SavingGameLogicManager.cs
+++ Assets/Script/SavingGameLogicManager.cs
@@ -3,36 +3,57 @@ using System;
 
 public class SavingGameLogicManager : MonoBehaviour
 {
+    public static SavingGameLogicManager Instance { get; private set; }
     [SerializeField] private int secondsInOneMonth = 5;
-
+    [SerializeField] private float startingCash = 1000f;
+    [SerializeField] private float cashPerMonth = 100f;
     public static event Action<int, int> OnNewMonth;
     // year, month
 
     private int currentMonth;
     private int currentYear;
     private float currentTime;
+    private float cash;
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
 
     private void Start()
     {
         currentMonth = 1;
         currentYear = 0;
         currentTime = 0f;
+        cash = startingCash;
+
     }
 
     private void Update()
     {
         currentTime += Time.deltaTime;
-
+        Debug.Log("Current Time: " + GetFullTimeInSeconds());
         if (currentTime >= secondsInOneMonth)
         {
-            currentTime = 0f;
             AdvanceMonth();
+            currentTime = 0f;
         }
     }
 
     private void AdvanceMonth()
     {
+        //TODO: clear UI notifications for new month //kf
+        //call function OncloseBankPanel in SavingGameUIManager //kf
         currentMonth++;
+        cash += cashPerMonth;
+        Debug.Log($"💰 Received monthly cash: {cashPerMonth}. Current cash: {cash}");
 
         if (currentMonth > 12)
         {
@@ -51,5 +72,30 @@ public class SavingGameLogicManager : MonoBehaviour
     public int GetCurrentYear()
     {
         return currentYear;
+    }
+    public float GetCurrentCash()
+    {
+        return cash;
+    }
+    public void AddCash(float amount)
+    {
+        cash += amount;
+    }
+    public bool DeductCash(float amount)
+    {
+        if (cash >= amount)
+        {
+            cash -= amount;
+            return true;
+        }
+        else
+        {
+            Debug.LogWarning("Not enough cash!");
+            return false;
+        }
+    }
+    public float GetFullTimeInSeconds()
+    {
+        return (currentYear * 12 + currentMonth - 1) * secondsInOneMonth + currentTime;
     }
 }
