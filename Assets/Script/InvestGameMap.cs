d9f039d9f83f93d717380be314ba8e094f944645
KHAWFANG07 <104263847+KHAWFANG07@users.noreply.github.com>
2026-01-22 21:33:23 +0700

    add settings button and update InvestGameMap UI and logic

--- Assets/Script/InvestGameMap.cs
+++ Assets/Script/InvestGameMap.cs
@@ -7,6 +7,10 @@ public class InvestGameMap : MonoBehaviour
     [SerializeField] private Button button2;
     [SerializeField] private Button button3;
     [SerializeField] private Button button4;
+    [SerializeField] private Button button5;
+    [SerializeField] private Button achievement;
+    [SerializeField] private Button leaderboard;
+    [SerializeField] private Button Setting;
 
     private void Start()
     {
@@ -14,10 +18,23 @@ public class InvestGameMap : MonoBehaviour
         button2.onClick.AddListener(() => SelectStage(2));
         button3.onClick.AddListener(() => SelectStage(3));
         button4.onClick.AddListener(() => SelectStage(4));
+        button5.onClick.AddListener(() => SelectStage(5));
+        achievement.onClick.AddListener(() => SelectItem("Achievement"));
+        leaderboard.onClick.AddListener(() => SelectItem("Leaderboard"));
+        Setting.onClick.AddListener(() => SelectItem("Setting"));
     }
 
     private void SelectStage(int stageNumber)
     {
         Debug.Log("Selected Stage: " + stageNumber);
     }
+    private void SelectItem(string stage)
+    {
+        Debug.Log("Go to" + stage);
+    }
+    private void SelectSetting (string stage)
+    {
+        Debug.Log("Go to" + stage);
+    }
+    
 }
