c42250286e570040cadedf2461e6cbc961276856
KHAWFANG07 <104263847+KHAWFANG07@users.noreply.github.com>
2026-03-06 08:31:08 +0700

    Convert tips to a prefab

--- Assets/Script/SceneLoader.cs
+++ Assets/Script/SceneLoader.cs
@@ -1,18 +1,51 @@
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 public class SceneLoader : MonoBehaviour
 {
-    public void LoadScene(string sceneName)
+    public GameObject saving;
+    public GameObject taxGameTips;
+    public GameObject icon;
+
+
+    void Start()
     {
-        SceneManager.LoadScene(sceneName);
+        icon.SetActive(true);
+        saving.SetActive(false);
+        taxGameTips.SetActive(false);
     }
 
-    public void LoadSavingTips()
+
+    public void ShowSaving()
     {
-        if (GameProgress.hardUnlocked)
-            SceneManager.LoadScene("SavingGameHardTips");
-        else
-            SceneManager.LoadScene("SavingGameEasyTips");
+        if (saving.activeSelf) return;
+
+        saving.SetActive(true);
+        taxGameTips.SetActive(false);
+    }
+
+    public void ShowTax()
+    {
+        if (taxGameTips.activeSelf) return;
+
+        taxGameTips.SetActive(true);
+        saving.SetActive(false);
+    }
+
+    // กด icon เพื่อเปิด Tips
+    public void OpenTips()
+    {
+        icon.SetActive(false);
+
+        saving.SetActive(false);       // เปิดหน้าแรก
+        taxGameTips.SetActive(true);
+    }
+
+    // กด exit เพื่อกลับไป icon
+    public void ExitTips()
+    {
+        icon.SetActive(true);
+
+        saving.SetActive(false);
+        taxGameTips.SetActive(false);
     }
 }
