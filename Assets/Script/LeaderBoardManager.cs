c845d4a7457a4ba1b2824e15c564a2af93db74c0
jirat <jiratchayaya303@gmail.com>
2026-03-07 21:25:12 +0700

    create LeaderBoard UI and fix LeaderBoard script

--- Assets/Script/LeaderBoardManager.cs
+++ Assets/Script/LeaderBoardManager.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 using TMPro; 
 using System.Threading.Tasks;
 using System.Collections.Generic;
+using System.Xml.Serialization;
 
 
 public class LeaderBoardManager : MonoBehaviour
@@ -14,11 +15,39 @@ public class LeaderBoardManager : MonoBehaviour
 
     private List<LeaderBoardEntry> cachedData;
 
-    private async void Start()
+    [Header("Panel Referance")]
+    [SerializeField] private GameObject leaderBoardPanel;
+
+    [Header("My Rank Bar")]
+    [SerializeField] private GameObject myRankBar;
+    [SerializeField] private LeaderBoardItemUI myRankItem;
+
+    public void OpenLeaderBoard()
+    {
+        leaderBoardPanel.SetActive(true);
+        RefreshData();
+    }
+
+    public void CloseLeaderBoard()
+    {
+        leaderBoardPanel.SetActive(false);
+    }
+
+    private async void RefreshData()
     {
         cachedData = await APIManager.Instance.GetLeaderBoardData();
+        UpdateDisplay(gameDropdown.value);
+    }
+
+    private async void Start()
+    {
+        await Task.Yield();
+        
+        cachedData = await APIManager.Instance.GetLeaderBoardData();
         gameDropdown.value = 0;
+        gameDropdown.onValueChanged.AddListener(OnDropdownChanged);
         UpdateDisplay(0);
+
     }
 
     public void OnDropdownChanged(int index)
@@ -28,22 +57,45 @@ public class LeaderBoardManager : MonoBehaviour
 
     public void UpdateDisplay(int index)
     {
-        // clear ui
+
         foreach (Transform child in container) Destroy(child.gameObject);
 
         if (cachedData == null || cachedData.Count == 0) return;
 
-        if(index == 0)// Saving Game
-            cachedData.Sort((a,b) => b.saving_game_score.CompareTo(a.saving_game_score));
-        else
-            cachedData.Sort((a,b) => b.tax_game_score.CompareTo(a.tax_game_score));
+        
+        List<LeaderBoardEntry> displayData = new List<LeaderBoardEntry>(cachedData);
 
-        // crate ui item
-        for(int i = 0; i < cachedData.Count; i++)
+        if (index == 0) 
+        {
+            displayData.Sort((a, b) => b.saving_game_score.CompareTo(a.saving_game_score));
+        }
+        else 
+        {
+            displayData.Sort((a, b) => b.tax_game_score.CompareTo(a.tax_game_score));
+        }
+
+        
+        for (int i = 0; i < displayData.Count; i++)
         {
             var item = Instantiate(rowPrefab, container).GetComponent<LeaderBoardItemUI>();
-            int displayScore = (index == 0) ? cachedData[i].saving_game_score : cachedData[i].tax_game_score;
-            item.SetData(i + 1, cachedData[i].username, displayScore);
+            int displayScore = (index == 0) ? displayData[i].saving_game_score : displayData[i].tax_game_score;
+            item.SetData(i + 1, displayData[i].username, displayScore);
         }
+
+        string myUsername = APIManager.myData.username;
+        for(int i = 0; i < displayData.Count; i++)
+        {
+            if(displayData[i].username == myUsername)
+            {
+                int myScore = (index == 0)
+                    ? displayData[i].saving_game_score
+                    : displayData[i].tax_game_score;
+
+                myRankBar.SetActive(true);
+                myRankItem.SetData(i + 1, displayData[i].username, myScore);
+                return;
+            }
+        }
+        myRankBar.SetActive(false);
     }
 }
