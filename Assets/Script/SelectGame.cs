bcde355b26b8f30bd039282221a0fe2abe478e44
KHAWFANG07 <104263847+KHAWFANG07@users.noreply.github.com>
2026-03-16 18:22:06 +0700

    Add stage completion system to Stage Info page

--- Assets/Script/SelectGame.cs
+++ Assets/Script/SelectGame.cs
@@ -1,20 +1,22 @@
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class SelectGame : MonoBehaviour
 {
     [SerializeField] private Button savinggame;
     [SerializeField] private Button taxgame;
     [SerializeField] private Button investgame;
-    
+
     private void Start()
     {
         savinggame.onClick.AddListener(() => SelectGameOption("Saving Game"));
-        taxgame.onClick.AddListener(() => SelectGameOption("Tax Game"));
+        taxgame.onClick.AddListener(() => SceneManager.LoadScene("TaxGameMap"));
         investgame.onClick.AddListener(() => SelectGameOption("Investment Game"));
     }
     private void SelectGameOption(string game)
     {
         Debug.Log($"Selected Game: {game}");
+
     }
 }
