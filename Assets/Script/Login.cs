24cb4bd838de36ab5e34f301b7c063817ee7589c
nut-natthawat <dfnmutthor@gmail.com>
2026-03-18 03:26:04 +0700

    Add login register password hiding and fix achievement to prefabs and add animation before exam and sound

--- Assets/Script/Login.cs
+++ Assets/Script/Login.cs
@@ -8,10 +8,47 @@ public class login : MonoBehaviour
     [SerializeField] private Button loginButton;
     [SerializeField] private Button registerButton;
 
+    [Header("Password Visibility Toggle")]
+    [SerializeField] private Button toggleEyeButton;
+    [SerializeField] private Image eyeIconImage;
+    [SerializeField] private Sprite eyeOpenSprite;
+    [SerializeField] private Sprite eyeClosedSprite;
+
+    private bool isPasswordVisible = false;
+
     private void Start()
     {
         loginButton.onClick.AddListener(HandleLogin);
         registerButton.onClick.AddListener(HandleRegister);
+
+        if (toggleEyeButton != null)
+        {
+            toggleEyeButton.onClick.AddListener(TogglePasswordVisibility);
+        }
+
+        passwordInput.contentType = TMP_InputField.ContentType.Password;
+        if (eyeIconImage != null && eyeClosedSprite != null) 
+            eyeIconImage.sprite = eyeClosedSprite;
+
+    }
+    private void TogglePasswordVisibility()
+    {
+        isPasswordVisible = !isPasswordVisible; 
+
+        if (isPasswordVisible)
+        {
+            passwordInput.contentType = TMP_InputField.ContentType.Standard;
+            if (eyeIconImage != null && eyeOpenSprite != null) 
+                eyeIconImage.sprite = eyeOpenSprite;
+        }
+        else
+        {
+            passwordInput.contentType = TMP_InputField.ContentType.Password;
+            if (eyeIconImage != null && eyeClosedSprite != null) 
+                eyeIconImage.sprite = eyeClosedSprite;
+        }
+
+        passwordInput.ForceLabelUpdate();
     }
     private async void HandleLogin()
     {
--- Assets/Script/SelectGame.cs
+++ Assets/Script/SelectGame.cs
@@ -6,14 +6,14 @@ public class SelectGame : MonoBehaviour
 {
     [SerializeField] private Button savinggame;
     [SerializeField] private Button taxgame;
-    [SerializeField] private Button achievement;
+    //[SerializeField] private Button achievement;
     [SerializeField] private Button leaderboard;
 
     private void Start()
     {
         savinggame.onClick.AddListener(() => SelectGameOption("SavingGameMap"));
         taxgame.onClick.AddListener(() => SelectGameOption("TaxGameMap"));
-        achievement.onClick.AddListener(() => SelectGameOption("Achievement"));
+        //achievement.onClick.AddListener(() => SelectGameOption("Achievement"));
         leaderboard.onClick.AddListener(() => SelectGameOption("LeaderBoardScene"));
     }
     private void SelectGameOption(string game)
