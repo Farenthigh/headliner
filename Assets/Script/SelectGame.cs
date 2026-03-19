5bbf18525a7bca66a6fa4e419c6f5922db4c713a
Farenthigh <naxx.iiax@gmail.com>
2026-03-20 06:26:16 +0700

    fix : Select Game, Saving game select stage, saving game save star into db

--- Assets/Script/SelectGame.cs
+++ Assets/Script/SelectGame.cs
@@ -1,7 +1,6 @@
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
-using UnityEngine.SceneManagement;
 
 public class SelectGame : MonoBehaviour
 {
@@ -10,6 +9,7 @@ public class SelectGame : MonoBehaviour
     [SerializeField] private Button leaderboard;
 
 
+    [SerializeField] private Button investgame;
 
     private void Start()
     {
