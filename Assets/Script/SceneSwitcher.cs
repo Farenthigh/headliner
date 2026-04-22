eeff38d2e539a822f07fee6de7f39f960cbff81f
Farenthigh <naxx.iiax@gmail.com>
2026-04-22 19:55:48 +0700

    add audio

--- Assets/Script/SceneSwitcher.cs
+++ Assets/Script/SceneSwitcher.cs
@@ -3,7 +3,7 @@ using UnityEngine.SceneManagement;
 
 public class SceneSwitcher : MonoBehaviour
 {
-    public string settingSceneName = "SettingsScene"; 
+    public string settingSceneName = "SettingsScene";
 
     public void GoToSettings()
     {
